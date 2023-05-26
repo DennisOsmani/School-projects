70ec4ccc22f026c24d710b0fbb28cec7ee247b54
D596242 <Dennisosmani@hotmail.com>
2023-05-26 12:02:00 +0200

    exchange buddy

--- CarRental/CarRental/src/main/java/oblig2/web/WebApplication2.java
+++ CarRental/CarRental/src/main/java/oblig2/web/WebApplication2.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
+		SpringApplication.run(WebApplication2.class, args);
 	}
 
 } 
--- CarRental/CarRental/src/main/java/oblig2/web/controller/DatabaseTester.java
+++ CarRental/CarRental/src/main/java/oblig2/web/controller/DatabaseTester.java
@@ -19,122 +19,122 @@ import oblig2.web.service.SearchService;
 @Controller
 public class DatabaseTester {
 	
-	@Autowired NewUserService us; 
-	@Autowired SearchService ss;
-	@Autowired RentalOfficeRepo roRepo;
-	@Autowired CarRentalService rs;
-	
-	
-	
-	/* TESTS SEARCH-SERVICE */
-
-	@GetMapping("/search")
-	public String get() {
-		
-		RentalOffice office = roRepo.findByOfficeid(1);
-		List<Car> li = ss.searchOffice(office);
-		
-		for(Car c : li) {
-			System.err.println(c.getBrand() + " " + c.getModel() + " : " + c.getRegnr());
-		}		
-		
-		return "homeView";
-	}
-	
-	
-	
-	/* TESTS NEW-USER-SERVICE USERS */
-	
-	@GetMapping("/adduser")
-	public String getA() {
-		
-		String number = "99226633";
-		Userr newUser = us.newUser(number, "Vendela", "Pilgaard", "passord", "morveien 69", "6969", "mordihula");
-		
-		return "homeView";
-	}
-	
-	
-	@GetMapping("/deluser")
-	public String getD() {
-		
-		String number = "99226633";
-		us.deleteUser(number);
-		
-		return "homeView";
-	}
-	
-	
-	
-	
-	/* TESTS CAR-RENTAL-SERVICE */
-	
-	@GetMapping("/getcar")
-	public String get1() {
-		
-		Car car = rs.getCar("EV55555");
-		System.err.println(car.getRegnr());
-		
-		return "homeView";
-	}
-	
-	@GetMapping("/issetavailable")
-	public String get2() {
-		
-		boolean a = rs.isCarAvailable("EV55555");
-		System.err.println("True: " + a);
-		
-		rs.setAvailability(false, "EV55555");
-		
-		// ETterpå
-		boolean b = rs.isCarAvailable("EV55555");
-		System.err.println("False: " + b);
-		
-		return "homeView";
-	}
-	
-	
-	@GetMapping("/getrentaluser")
-	public String get4() {
-		
-		
-		return "homeView";
-	}
-	
-	@GetMapping("/getrental")
-	public String get5() {
-		
-		Rental r = rs.getRental(1);
-		System.err.println(r.getCreditcard());
-		
-		return "homeView";
-	}
-	
-	@GetMapping("/getrentaloffice")
-	public String get6() {
-		
-		RentalOffice ro = rs.getRentalOffice(1);
-		System.err.println(ro.getName());
-		
-		return "homeView";
-	}
-	
-	@GetMapping("/newrental")
-	public String get7() {
-		
-		rs.newRental("55556666", "EV55555", "1234 1234 1234 1234", "0", null, null);
-		
-		return "homeView";
-	}
-	
-	@GetMapping("/deliverrental")
-	public String get8() {
-		
-		rs.deliverRental("55556666", "EV55555", "20000");
-		
-		return "homeView";
-	}
-	
+//	@Autowired NewUserService us; 
+//	@Autowired SearchService ss;
+//	@Autowired RentalOfficeRepo roRepo;
+//	@Autowired CarRentalService rs;
+//	
+//	
+//	
+//	/* TESTS SEARCH-SERVICE */
+//
+//	@GetMapping("/search")
+//	public String get() {
+//		
+//		RentalOffice office = roRepo.findByOfficeid(1);
+//		List<Car> li = ss.searchOffice(office);
+//		
+//		for(Car c : li) {
+//			System.err.println(c.getBrand() + " " + c.getModel() + " : " + c.getRegnr());
+//		}		
+//		
+//		return "homeView";
+//	}
+//	
+//	
+//	
+//	/* TESTS NEW-USER-SERVICE USERS */
+//	
+//	@GetMapping("/adduser")
+//	public String getA() {
+//		
+//		String number = "99226633";
+//		Userr newUser = us.newUser(number, "Vendela", "Pilgaard", "passord", "morveien 69", "6969", "mordihula");
+//		
+//		return "homeView";
+//	}
+//	
+//	
+//	@GetMapping("/deluser")
+//	public String getD() {
+//		
+//		String number = "99226633";
+//		us.deleteUser(number);
+//		
+//		return "homeView";
+//	}
+//	
+//	
+//	
+//	
+//	/* TESTS CAR-RENTAL-SERVICE */
+//	
+//	@GetMapping("/getcar")
+//	public String get1() {
+//		
+//		Car car = rs.getCar("EV55555");
+//		System.err.println(car.getRegnr());
+//		
+//		return "homeView";
+//	}
+//	
+//	@GetMapping("/issetavailable")
+//	public String get2() {
+//		
+//		boolean a = rs.isCarAvailable("EV55555");
+//		System.err.println("True: " + a);
+//		
+//		rs.setAvailability(false, "EV55555");
+//		
+//		// ETterpå
+//		boolean b = rs.isCarAvailable("EV55555");
+//		System.err.println("False: " + b);
+//		
+//		return "homeView";
+//	}
+//	
+//	
+//	@GetMapping("/getrentaluser")
+//	public String get4() {
+//		
+//		
+//		return "homeView";
+//	}
+//	
+//	@GetMapping("/getrental")
+//	public String get5() {
+//		
+//		Rental r = rs.getRental(1);
+//		System.err.println(r.getCreditcard());
+//		
+//		return "homeView";
+//	}
+//	
+//	@GetMapping("/getrentaloffice")
+//	public String get6() {
+//		
+//		RentalOffice ro = rs.getRentalOffice(1);
+//		System.err.println(ro.getName());
+//		
+//		return "homeView";
+//	}
+//	
+//	@GetMapping("/newrental")
+//	public String get7() {
+//		
+//		rs.newRental("55556666", "EV55555", "1234 1234 1234 1234", "0", null, null);
+//		
+//		return "homeView";
+//	}
+//	
+//	@GetMapping("/deliverrental")
+//	public String get8() {
+//		
+//		rs.deliverRental("55556666", "EV55555", "20000");
+//		
+//		return "homeView";
+//	}
+//	
 	
 	
 }
